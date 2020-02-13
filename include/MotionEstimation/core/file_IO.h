3c59a435dd8c801c0f2418b062c114e5c89a99c2
Axel Beauvisage <axel.beauvisage@gmail.com>
2020-02-13 22:22:09 +0100

    Add new config parameter called video
    This new parameters allows to specify if the input camera feed is a video
    or an a sequence of images.
    
    A new cv_sig_handler struct name is replaced by CvSigHandler to avoid
    confusion (it is a type and not a variable).

--- include/MotionEstimation/core/file_IO.h
+++ include/MotionEstimation/core/file_IO.h
@@ -40,6 +40,7 @@ enum class FilterType{EKF,EKFE,MREKF,RCEKF,Linear,None};
     - lframe: number of the last frame
     - skip: number of frame skipped between two consecutive frame (define the processing frequency) */
 struct FrameInfo{
+
     int fframe = 0;
     int lframe = 0;
     int skip = 1;
@@ -47,8 +48,10 @@ struct FrameInfo{
     int init = 0;
 
     void write(cv::FileStorage& fs) const{
-        fs << "{" << "start" << fframe << "stop" << lframe << "rate" << skip << "bframe" << bias_frame << "initframe" << init << "}";
+        fs  << "{" << "start" << fframe << "stop" << lframe << "rate" << skip
+            << "bframe" << bias_frame << "initframe" << init << "}";
     }
+
     void read(const cv::FileNode& node){
         fframe = (int)node["start"];
         lframe = (int)node["stop"];
@@ -62,6 +65,7 @@ struct FrameInfo{
 
 //! structure containing tracking parameters
 struct TrackingInfo{
+
     int nb_feats=500;
     int window_size=5;
     int ba_rate=0;
@@ -69,8 +73,12 @@ struct TrackingInfo{
     double feat_cov = 0.25;
 
     void write(cv::FileStorage& fs) const{
-        fs << "{" << "feats" << nb_feats << "window" << window_size << "ba_rate" << ba_rate << "parallax" << parallax << "feat_cov" << feat_cov << "}";
+        fs  << "{"
+            << "feats" << nb_feats << "window" << window_size << "ba_rate"
+            << ba_rate << "parallax" << parallax << "feat_cov" << feat_cov
+            << "}";
     }
+
     void read(const cv::FileNode& node){
         nb_feats = (int)node["feats"];
         window_size = (int)node["window"];
@@ -84,10 +92,12 @@ struct TrackingInfo{
 
 //! structure containing various information about the dataset to use
 struct DatasetInfo{
+
     std::string dir="";
     std::string image_filename="";
     std::string gt_filename="";
     std::string imu_filename="";
+    bool is_video = false;
     double gps_orientation = 0;
     SetupType type=SetupType::mono;
     bool scaled_traj=false;
@@ -95,20 +105,28 @@ struct DatasetInfo{
     int cam_ID=0;
     Quatd q_init, q_cam_to_base;
     cv::Vec3d p_init = cv::Vec3d(0,0,0), p_cam_to_base = cv::Vec3d(0,0,0);
+
     void write(cv::FileStorage& fs) const{
 		fs	<< "{"
-			<< "dir" << dir << "image_file" << image_filename << "gt_file" << gt_filename << "imu_file" << imu_filename
-			<< "gps" << gps_orientation << "type" << (type==SetupType::mono?"mono":"stereo")
-			<< "scaled" << (scaled_traj?"true":"false") << "poses" << (poses==PoseType::ABSOLUTE?"absolute":"relative")
-			<< "camID" << cam_ID << "init_orientation" << q_init.getCoeffs() << "init_position" << p_init
-			<< "cam_orientation" << q_cam_to_base.getCoeffs() << "cam_position" << p_cam_to_base
+			<< "dir" << dir << "image_file" << image_filename << "gt_file"
+            << gt_filename << "imu_file" << imu_filename << "video"
+            << (is_video==true?"true":"false") << "gps" << gps_orientation
+            << "type" << (type==SetupType::mono?"mono":"stereo")
+			<< "scaled" << (scaled_traj?"true":"false") << "poses"
+            << (poses==PoseType::ABSOLUTE?"absolute":"relative") << "camID" << cam_ID
+            << "init_orientation" << q_init.getCoeffs() << "init_position" << p_init
+			<< "cam_orientation" << q_cam_to_base.getCoeffs() << "cam_position"
+            << p_cam_to_base
 			<< "}";
     }
+
     void read(const cv::FileNode& node){
+
         dir = (std::string)node["dir"];
         image_filename = (std::string)node["image_file"];
         gt_filename = (std::string)node["gt_file"];
         imu_filename = (std::string)node["imu_file"];
+        is_video = (node["video"]=="true"?true:false);
         gps_orientation = (double)node["gps"];
         type = (node["type"]=="mono"?SetupType::mono:SetupType::stereo);
         scaled_traj = (node["scaled"]=="true"?true:false);
@@ -138,7 +156,7 @@ void read(const cv::FileNode& node, T& x, const T& default_value = T()){
         x.read(node);
 }
 
-struct cv_sig_handler{
+struct IOSigHandler{
 
     int _break_=0;
     bool _quit_=false;
@@ -154,8 +172,7 @@ struct cv_sig_handler{
                 case 'p':
                     _break_ = 0;break;
                 case 'r':
-                _break_
-                = 10;break;
+                _break_ = 10;break;
                 case 'q':
                     _quit_ = true;
 
@@ -182,14 +199,24 @@ bool loadYML(std::string filename);
 //! returns the image(s) corresponding to the frame number
 /*! if monocular, the second image of the pair is empty. */
 std::pair<cv::Mat,cv::Mat> loadImages(const std::string& dir, int nb);
-void loadImagesKitti(const std::string& dir, int nb, std::pair<cv::Mat,cv::Mat>& imgs, const int padding=8);
-cv::Mat loadImageKitti(const std::string& dir, int cam_nb, int img_nb, const int padding=8);
-void loadImages(const std::string& dir, int nb, std::pair<cv::Mat,cv::Mat>& imgs, const int padding=5);
+void loadImagesKitti(   const std::string& dir, int nb,
+                        std::pair<cv::Mat,cv::Mat>& imgs, const int padding=8);
+cv::Mat loadImageKitti( const std::string& dir, int cam_nb, int img_nb,
+                        const int padding=8);
+void loadImages(const std::string& dir, int nb, std::pair<cv::Mat,cv::Mat>& imgs,
+                const int padding=5);
 cv::Mat loadImage(const std::string& dir, int cam_nb, int img_nb, const int padding=5);
-void loadPCImages(const std::string& dir, int nb, std::vector<std::pair<cv::Mat,cv::Mat>>& imgs, const int padding=5);
-std::vector<cv::Mat> loadPCImage(const std::string& dir, int cam_nb, int img_nb, const int padding=5);
+void loadPCImages(  const std::string& dir, int nb,
+                    std::vector<std::pair<cv::Mat,cv::Mat>>& imgs, const int padding=5);
+std::vector<cv::Mat> loadPCImage(   const std::string& dir, int cam_nb, int img_nb,
+                                    const int padding=5);
 
-inline void openLogFile(const std::string& filename){logFile.open(filename);if(!logFile.is_open())std::cerr << "could not create log file" << std::endl;}
+inline void openLogFile(const std::string& filename){
+    logFile.open(filename);
+    if(!logFile.is_open())
+        std::cerr << "could not create log file" << std::endl;
+}
+
 //! write a string in the logFile. Useful for displaying data without flooding the standard output.
 inline void writeLogFile(const std::string& message){logFile << message;}
 inline void closeLogFile(){logFile.close();}
@@ -197,15 +224,18 @@ inline void closeLogFile(){logFile.close();}
 class IOFile{
 
 public:
+
     IOFile():m_filename(""){}
     IOFile(std::string filename):m_filename(filename){openFile(m_filename);}
     ~IOFile(){m_file.close();}
+
     bool is_open(){return m_file.is_open();}
     int openFile(std::string filename);
     void closeFile(){m_file.close();}
     std::string show_header(){std::string header;for(auto s : m_file_desc)header+=s+';';return header;}
 
 private:
+
     int check_header();
 
 protected:
@@ -213,12 +243,12 @@ protected:
     std::string m_filename;
     std::ifstream m_file;
     std::vector<std::string> m_file_desc;
-
 };
 
 class ImuFile : public IOFile{
 
 public:
+
     ImuFile():IOFile(){}
     ImuFile(std::string filename):IOFile(filename){}
     //! reads the one ImuData (the next one in the file).
@@ -227,12 +257,12 @@ public:
     //! get the first ImuData after the provided timestamp.
     /*! Corresponds to the average of all the imu data between current position in the file and the first imu data after the timestamp.*/
     int getNextData(int64_t stamp, ImuData& data);
-
 };
 
 class GpsFile : public IOFile{
 
 public:
+
     GpsFile():IOFile(){}
     GpsFile(std::string filename):IOFile(filename){}
     //! reads the one GpsData (the next one in the file).
@@ -240,12 +270,12 @@ public:
     int readData(GpsData& data);
     //! get the first GpsData after the provided timestamp.
     int getNextData(int64_t stamp, GpsData& data);
-
 };
 
 class PoseFile : public IOFile{
 
 public:
+
     PoseFile():IOFile(){}
     PoseFile(std::string filename):IOFile(filename){}
     //! reads the one PoseData (the next one in the file).
@@ -253,18 +283,17 @@ public:
     int readData(PoseData& data);
     //! get the first PoseData after the provided timestamp.
     int getNextData(int64_t stamp, PoseData& data);
-
 };
 
 class ImageFile : public IOFile{
 
 public:
+
     ImageFile():IOFile(){}
     ImageFile(std::string filename):IOFile(filename){}
     //! reads the one ImageData (the next one in the file).
     /*! returns the number of the image and its corresponding timestamp.*/
     int readData(int& nb, int64_t& stamp);
-
 };
 
 //! structure to read images in a directoty or video
@@ -278,15 +307,13 @@ struct ImageReader{
     int img_nb; //!< current image number
     int64_t img_stamp; //! current_image_stamp
 
-
     ImageReader(const std::string& filename="", Type type_=Type::IMAGES): fimage{filename}, cap(dataset_info.type==SetupType::stereo?2:1),type{type_},img_nb{0},img_stamp{0}{
-
         if(!fimage.is_open())
             std::cerr << "[ImageReader] warning: could not find an image file in " << dataset_info.dir << std::endl;
 
         if(type == Type::VIDEO){ // if video needs to open video streams
             for(uint i=0;i<cap.size();i++){
-                cap[i].open(dataset_info.dir+"/cam"+std::to_string(i)+"_image.mp4");
+                cap[i].open(dataset_info.dir+"/cam"+std::to_string(i)+"_image.avi");
             }
         }
         do{
@@ -295,20 +322,18 @@ struct ImageReader{
     }
 
     void openReader(const std::string& filename=dataset_info.dir+"/image_data.csv", Type type_=Type::IMAGES){
-
         fimage.openFile(filename);
         type = type_;
 
         if(!fimage.is_open())
             std::cerr << "[ImageReader] warning: could not find an image file in " << dataset_info.dir << std::endl;
-
-        if(type == Type::VIDEO){ // if video needs to open video streams
+        else if(type == Type::VIDEO){ // if video needs to open video streams
             for(uint i=0;i<cap.size();i++){
-                cap[i].open(dataset_info.dir+"/cam"+std::to_string(i)+"_image.mp4");
+                cap[i].open(dataset_info.dir+"/cam"+std::to_string(i)+"_image.avi");
             }
         }
         do{
-          readMono(); // this method can be run  with mono and stereo
+          readMono(); // this method can be run with mono and stereo
         }while(img_nb<frame_info.fframe); // reading images until we reach the first frame
     }
 
@@ -339,15 +364,22 @@ struct ImageReader{
           return loadImages(dataset_info.dir,img_nb);
       else{
         std::pair<cv::Mat,cv::Mat> imgs;
+        if(!cap[0].isOpened() || !cap[1].isOpened()){
+            std::cout << "[ImageReader] Warning: video stream is not opened!"<< std::endl;
+            return imgs;
+        }
+
         while(cap[0].get(cv::CAP_PROP_POS_FRAMES) < img_nb)
 			cap[0].grab();
 		while(cap[1].get(cv::CAP_PROP_POS_FRAMES) < img_nb)
 			cap[1].grab();
 		cap[0] >> imgs.first; cap[1] >> imgs.second;
+
         if(imgs.first.type() > 8)
             cv::cvtColor(imgs.first,imgs.first,CV_BGR2GRAY);
         if(imgs.second.type() > 8)
             cv::cvtColor(imgs.second,imgs.second,CV_BGR2GRAY);
+
         return imgs;
       }
     }
@@ -370,11 +402,19 @@ struct ImageReader{
           return loadImage(dataset_info.dir,dataset_info.cam_ID,img_nb);
       else{
         cv::Mat img;
+
+        if(!cap[0].isOpened()){
+            std::cout << "[ImageReader] Warning: video stream is not opened!"<< std::endl;
+            return img;
+        }
+
         while(cap[0].get(cv::CAP_PROP_POS_FRAMES) < img_nb)
 			cap[0].grab();
+
          cap[0] >> img;
         if(img.type() > 8)
             cv::cvtColor(img,img,CV_BGR2GRAY);
+
         return img;
       }
     }
